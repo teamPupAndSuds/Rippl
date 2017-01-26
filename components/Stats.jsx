5c33a4026925f3f111ce4977aeb446590747eeec
Jeremy Sande <sandejeremy@gmail.com>
2017-01-26 12:14:21 -0800

    add components folder, template implementation of statsscreen

---
+++ components/Stats.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+
+export default class stats extends Component {
+
+}
+
+
+AppRegistry.registerComponent('stats', () => stats);
--- index.ios.js
+++ index.ios.js
@@ -5,6 +5,7 @@
  */
 
 import React, { Component } from 'react';
+// import stats from './components/Stats.jsx';
 import {
   AppRegistry,
   StyleSheet,
@@ -13,21 +14,41 @@ import {
 } from 'react-native';
 
 export default class test extends Component {
+  constructor() {
+    super();
+    this.state = {
+      statsScreen: false
+    }
+  }
+
+  statsScreen = () => {
+    this.setState({statsScreen: true});
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
+    if (!this.state.statsScreen) {
+      return (
+        <View style={styles.container}>
+          <Text style={styles.welcome}>
+            Welcome to Rippl!
+          </Text>
+          <Text style={styles.instructions}>
+            To get started, edit index.ios.js
+          </Text>
+          <Text style={styles.instructions}>
+            Press Cmd+R to reload,{'\n'}
+            Cmd+D or shake for dev menu
+          </Text>
+          <View>
+            <Text onPress={this.statsScreen}>StatsScreen</Text>
+          </View>
+        </View>
+      );
+    } else {
+      // return (
+        // <stats />
+      // );
+    }
   }
 }
 
