2c1f6982602e79d21eb07adce3a9cbbb33337d5b
Michael Tutt <tutt.michael@gmail.com>
2017-01-29 16:41:58 -0800

    Add config file for API keys

---
+++ server/config/twitter.example.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+  'consumer_key': 'APP_CONSUMER_KEY',
+  'consumer_secret': 'APP_CONSUMER_SECRET',
+  'access_token': 'USER_TOKEN',
+  'access_token_secret': 'USER_TOKEN_SECRET',
+};
