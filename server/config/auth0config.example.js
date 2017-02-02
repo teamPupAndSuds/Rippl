e301164b19141016edd3bb9badc1a7b922741006
Derrick Chan <dachanman91@gmail.com>
2017-02-01 16:41:56 -0800

    Add auth0config.example.js

---
+++ server/config/auth0config.example.js
@@ -0,0 +1,7 @@
+// Save down keys from auth0
+// Save this file down without example portion of filename, auth0config.js has been added to gitignore file
+// in order to protect keys from being uploaded
+module.exports = {
+  'key': 'AUTH0-KEY',
+  'url': 'rippl.auth0.com'
+};
