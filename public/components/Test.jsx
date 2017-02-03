a59854eb498ebed7ff540fef806c3ead8344b351
Jeremy Sande <sandejeremy@gmail.com>
2017-02-03 15:19:43 -0800

    added a test component

---
+++ public/components/Test.jsx
@@ -0,0 +1,16 @@
+import App from './App.jsx';
+
+class Test extends React.Component{
+  constructor(props){
+  	super(props);
+  }
+
+  render(){
+  	return(
+  	  <div>
+
+  	  </div>
+  	);
+  }
+}
+export default Test;
